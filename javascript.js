5a7b7aab5cf29501094b3c6dd4bb8f71a8346717
Brian Todd <44840863+BrianTodd092@users.noreply.github.com>
2022-01-02 23:07:46 +0000

    Add files via upload
    Array. Add or Remove multi line comment /* */  or single line comment //.

---
+++ javascript.js
@@ -0,0 +1,32 @@
+var array =["Sunday",  "Monday",  "Tuesday",  "Wednesday",  "Thursday",  "Friday","Saturday"]
+var date = new Date();
+var today = date.getDay();
+var msg = "";
+var i = 0;
+
+
+
+
+// Vertical Loop.
+/*for( i=0; i <  array.length ; i++){
+ msg += array[i] + "<br>";
+}*/
+
+
+//document.getElementById('weekday').innerHTML=array; // Horizontal.
+//document.getElementById('weekday').innerHTML=array.join(', ');  // Horizontal + Space.
+//document.getElementById('weekday').innerHTML=array.join(" <br> "); // Vertical.
+
+while (i < array.length)
+{
+	if(i=== today){
+		msg += '<p class="pa">' + array[i] + ' .... today! </p>';
+	}
+	else{
+	 msg += '<p class="pb">' + array[i] + '</p>';	
+	}
+	i++;
+}
+
+document.getElementById('weekday').innerHTML=msg;
+
